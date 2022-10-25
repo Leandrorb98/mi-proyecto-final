ac622bd975e7f131b22470b7194d5320840d7a85
Leandro Ruiz <leandroruiz5398@gmail.com>
2022-10-25 20:52:06 -0300

    commit entregable

--- ejemplo/views.py
+++ ejemplo/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
+from ejemplo.models import Familiar
 
 def index(request):
-    return render(request, "ejemplo/saludar.html")
+    return render(request, "ejemplo/saludar.html",{"nombre":"Leandro"})
 
 
+def monstrar_familiares(request):
+  lista_familiares = Familiar.objects.all()
+  return render(request, "ejemplo/familiares.html", {"lista_familiares": lista_familiares})
 
-    
 # Create your views here.
