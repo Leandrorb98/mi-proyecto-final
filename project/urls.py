a55493b0593a4ebc1e2745e5d8cb37c80b1ecf26
Leandro Ruiz <leandroruiz5398@gmail.com>
2022-11-03 23:30:44 -0300

    playground

--- project/urls.py
+++ project/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from ejemplo.views import (index, monstrar_familiares,index_dos, index_tres, BuscarFamiliar, AltaFamiliar)
+from ejemplo.views import (index, monstrar_familiares,index_dos, index_tres, BuscarFamiliar, AltaFamiliar,FamiliarList, FamiliarCrear, ActualizarFamiliar)
 from blog.views import index as blog_index
 
 urlpatterns = [
@@ -26,5 +26,9 @@ urlpatterns = [
     path('mostrar-notas/',index_tres),
     path('blog/',blog_index),
     path('mi-familia/buscar',BuscarFamiliar.as_view()),
-    path('mi-familia/alta', AltaFamiliar.as_view())
+    path('mi-familia/alta', AltaFamiliar.as_view()),
+    path('panel-familia/', FamiliarList.as_view()),
+    path('panel-familia/crear', FamiliarCrear.as_view()),
+    path('panel-familia/<int:pk>/borrar', FamiliarBorrar.as_view()),
+    path('panel-familia/<int:pk>/actualizar', FamiliarActualizar.as_view()),
 ]
