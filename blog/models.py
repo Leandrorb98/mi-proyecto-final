f690aa327b4fd6cb512883dddde43a205b02963f
Leandro Ruiz <leandroruiz5398@gmail.com>
2022-11-06 23:18:50 -0300

    playground dos

--- blog/models.py
+++ blog/models.py
@@ -3,5 +3,16 @@ from django.db import models
 class Configuracion(models.Model):
     nombre_blog = models.CharField(max_length=14)
 
+class Post(models.Model):
+    title=models.CharField(max_length=100)
+    short_content=models.CharField(max_length=255)
+    content=models.TextField(max_length=3000)
+    date_published=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"id:{self.id}, title:{self.title}"
+
+
+
 
 # Create your models here.
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from blog.models import Configuracion
+from django.views.generic import ListView, CreateView, DeleteView, UpdateView, DetailView
+from blog.models import Post
 def index (request):
     configuracion = Configuracion.objects.first()
     return render(request, 'blog/index.html',{'configuracion':configuracion})
 
+class ListPost(ListView):
+    model=Post
+
+class ListPost(ListView):
+    model=Post
 # Create your views here.
